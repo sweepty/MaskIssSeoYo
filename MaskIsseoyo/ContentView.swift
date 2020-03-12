fef37382ad8b6cbe0cdeb575831852f8f9544b7c
sweepty <adie0423@gmail.com>
2020-03-12 23:01:23 +0900

    Initial Commit

---
+++ MaskIsseoyo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MaskIsseoyo
+//
+//  Created by Seungyeon Lee on 2020/03/12.
+//  Copyright © 2020 Seungyeon Lee. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
